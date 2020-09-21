8560e367755bf8760e58fcd58b50a313355b7e55
elee2001 <hoyin_3@hotmail.com>
2020-09-20 22:07:20 -0700

    Worked a bit on the JS, need to choose way of getting words

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.onkeydown = function(e) {
+    if (e.key === 'Enter')
+    {
+        var input = document.getElementById("input");
+        //CHECK if this is one of the words
+
+        input.value = "";
+
+    }
+}
+
+function startGame() {
+    // document.getElementById("input").value = "check";
+    //set all the h2s with words scraped from somewhere?!
+}
+
