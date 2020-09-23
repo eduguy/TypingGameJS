c759d123abed55d722072bed7d8898eb366ad82b
elee2001 <hoyin_3@hotmail.com>
2020-09-22 21:44:37 -0700

    Added timers, basic checking of inputs, added todos

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+//TODO: find a way to scrape words from somewhere or create a more exhaustive list
+let words = ['abc', 'bac'];
+let score = 0;
+let time = 30;
+let gameOn=false;
+
 
 document.onkeydown = function(e) {
     if (e.key === 'Enter')
-    {
-        var input = document.getElementById("input");
-        //CHECK if this is one of the words
-        //if so, points++ and change it to a new word
+    {   
+        if (gameOn) {
 
+            let input = document.getElementById("input");
+            let val = input.value.trim();
+            let check = words.indexOf(val);
+            if (check !== -1) {
+                //TODO: remove word from the grid and replace it with next word
+                score++;
+                document.getElementById("score").innerHTML = "Current Score: " + score;
+                words.splice(check,1);
+            }            
+        }
         input.value = "";
 
+
     }
+
 }
 
 function startGame() {
-    decrementTime()
-    // document.getElementById("input").value = "check";
-    //set all the h2s with words scraped or hardcoded?
+    gameOn = true;
+    setInterval(decrementTime, 1000);
+    setTimeout(endGame, 30000);
 }
 
 function decrementTime() {
-    //decrement time by 1 if time is not already 0
-    //once 0, end hte game
+        time = time-1;
+        document.getElementById("timeLeft").innerHTML="TIME LEFT: " + time;
+}
+
+function endGame() {
+    //TODO: disable text box, return score
 }
