b40241741b9e8e17d5eb95f4a5129359378759c8
elee2001 <hoyin_3@hotmail.com>
2020-09-23 18:01:45 -0700

    Started to figure out the board setup

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //TODO: find a way to scrape words from somewhere or create a more exhaustive list
-let words = ['abc', 'bac'];
+let words = ['abc', 'bac', 'climb', 'rock', 'calculator', 'fds', 'fsa', 'bike', 'fork'];
+let active = [];
 let score = 0;
 let time = 30;
 let gameOn=false;
@@ -28,10 +29,35 @@ document.onkeydown = function(e) {
 }
 
 function startGame() {
+    
+    initGrid();
     gameOn = true;
     setInterval(decrementTime, 1000);
     setTimeout(endGame, 30000);
 }
+//whenever you do an updating move (scoring, initilizng), call update word to match
+//the array with the text
+function initGrid() {
+    let i = 1;
+    while (i <= 9) {
+        //TODO: no duplicates
+        //rand from 1 to words.length
+        let rand = Math.floor(Math.random() * words.length);
+        //str should be pulled from the words and moved to active
+        let str = words[rand];
+        words.splice(rand, 1);
+        active.push(str);
+        // document.getElementById("word" + i).innerHTML = str;
+        i = i + 1;
+    }
+
+    updateGrid;
+}
+function updateGrid() {
+    document.getElementById("word1").innerHTML = active[0];
+    //TODO: ETCCCC
+}
+
 
 function decrementTime() {
         time = time-1;
