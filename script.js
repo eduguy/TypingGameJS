49ea15b89ac7e47600a9e1560129994d0d791626
elee2001 <hoyin_3@hotmail.com>
2020-10-03 20:06:21 -0700

    Added more words, games has  most of the functionality!

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-//TODO: find a way to scrape words from somewhere or create a more exhaustive list
-let words = ['abc', 'bac', 'climb', 'rock', 'calculator', 'fds', 'fsa', 'bike', 'fork', 'mea', '123', 'aae', 
-'dog','cat','paper', 'phone', 'pen', 'pencil', 'cube', 'square'];
+//words taken from the first paragraph of: https://en.wikipedia.org/wiki/JavaScript
+let words = ["a","abbreviated","all","alongside","also","although","an","and","any","apis","application","applications","are","as","behavior","between","browser","browsers","but","client-side","compiled","conforms","cordova","core","created","css","curly-bracket","data","dates","dedicated","design","differ","distinct","document","does","dom","dynamic","ecmascript","electron","embedded","enables","engine","engines","environment","essential","event-driven","execute","expressions","facilities","first-class","for","frameworks","functional","functions","graphics","greatly","has","have","high-level","host","however","html","i","imperative","in","include","including","input","interactive","interfaces","is","it","itself","java","javascript","js","just-in-time","language","languages","libraries","major","majority","model","multi-paradigm","name","networking","node","not","now","object","object-orientation","of","often","one","only","or","originally","output","page","pages","part","programming","prototype-based","provides","pt","regular","respective","servers","similarities","skr","some","specification","standard","storage","structures","styles","such","supports","syntax","technologies","text","that","the","there","they","those","to","two","typing","use","used","usually","variety","vast","via","web","websites","were","wide","with","working","world"]
 let active = [];
 let score = 0;
 const wordTime = 4000;
 const fadeTime = 3000;
-let time = 20; //seconds
+const gameLength = 20;
+let time = 0; //seconds
 let gameOn=false;
-let interval;
 let missedWords=0;
 
 document.onkeydown = function(e) {
@@ -25,8 +24,9 @@ document.onkeydown = function(e) {
                 active[indexOfWord]= str;
                 words.splice(rand, 1);
                 switchVal(indexOfWord);
+
                 score++;
-                document.getElementById("score").innerHTML = "Current Score: " + score;
+                document.getElementById("score").innerHTML = "Current Score: " + (score-missedWords);
             }            
         }
         input.value = "";
@@ -51,8 +51,7 @@ function fade(val, origText, indexOfWord) {
     //if i pass the oriignal text here i can compare it?
     if (active[indexOfWord] === origText) {
         val.style.color = "red";
-        // setTimeout(function(){ val.style.color="black"; }, 1000)
-        //every word that comes in will rset color to black
+
     }
 }
 
@@ -68,16 +67,24 @@ function newWordTimeOut(indexOfWord, origText) {
         document.getElementById("word"+(indexOfWord+1)).style.color="black";
         words.splice(rand, 1);
         switchVal(indexOfWord);
+        document.getElementById("score").innerHTML = "Current Score: " + (score-missedWords);
+
     }
 }
 
 function startGame() {
     
     initGrid();
+    document.getElementById("input").value="";
+
+    time = gameLength;
+    score=0;
+    missedWords=0;
+    document.getElementById("score").innerHTML = "Current Score: " + (score-missedWords);
     document.getElementById("timeLeft").innerHTML="TIME LEFT: " + time;
     gameOn = true;
     document.getElementById("input").focus();
-    interval = setInterval(decrementTime, 1000);
+    setInterval(decrementTime, 1000);
     setTimeout(endGame, time*1000);
 }
 
@@ -91,14 +98,31 @@ function initGrid() {
         i = i + 1;
     }
     document.getElementById("word1").innerHTML = active[0];
+    setTimeout(fadeOut, 1000, document.getElementById("word1"), 0)
     document.getElementById("word2").innerHTML = active[1];
+    setTimeout(fadeOut, 1000, document.getElementById("word2"), 1)
+
     document.getElementById("word3").innerHTML = active[2];
+    setTimeout(fadeOut, 1000, document.getElementById("word3"), 2)
+
     document.getElementById("word4").innerHTML = active[3];
+    setTimeout(fadeOut, 1000, document.getElementById("word4"), 3)
+
     document.getElementById("word5").innerHTML = active[4];
+    setTimeout(fadeOut, 1000, document.getElementById("word5"), 4)
+
     document.getElementById("word6").innerHTML = active[5];
+    setTimeout(fadeOut, 1000, document.getElementById("word6"), 5)
+
     document.getElementById("word7").innerHTML = active[6];
+    setTimeout(fadeOut, 1000, document.getElementById("word7"), 6)
+
     document.getElementById("word8").innerHTML = active[7];
+    setTimeout(fadeOut, 1000, document.getElementById("word8"), 7)
+
     document.getElementById("word9").innerHTML = active[8];
+    setTimeout(fadeOut, 1000, document.getElementById("word9"), 8)
+
 }
 
 
@@ -110,7 +134,18 @@ function decrementTime() {
 
 function endGame() {
     gameOn = false;
-    clearInterval(interval);
-    let finalScore = score-missedWords;
-    window.alert("Your final score (minus missed words) is: "+finalScore)
+    clearAll();
+    window.alert("You typed "+score +" words in " + gameLength + " seconds for a WPM of: " + (score/gameLength));
+    document.getElementById("score").innerHTML = "Final Score: " + (score-missedWords);
+
+
+
 }
+//https://stackoverflow.com/questions/3141064/how-to-stop-all-timeouts-and-intervals-using-javascript
+function clearAll() {
+    for (var i = setTimeout(function() {}, 0); i > 0; i--) {
+      window.clearInterval(i);
+      window.clearTimeout(i);
+      if (window.cancelAnimationFrame) window.cancelAnimationFrame(i);
+    }
+  }
